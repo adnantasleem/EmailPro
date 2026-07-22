fb395f931c06f5dae80ee9a73da4a2523366e83b
Adnan Tasleem <64993416+adnantasleem@users.noreply.github.com>
2026-07-22 21:31:13 +0500

    Fix SMTP dropdown closing when clicking search input

--- resources/views/campaigns/show.blade.php
+++ resources/views/campaigns/show.blade.php
@@ -593,8 +593,8 @@
                             
                             <div class="relative">
                                 <label class="block text-sm font-medium text-gray-700 mb-1">SMTP Account</label>
-                                <div class="relative">
-                                    <button type="button" @click="openSmtp = !openSmtp" @click.away="openSmtp = false" class="relative w-full bg-white border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                                <div class="relative" @click.away="openSmtp = false">
+                                    <button type="button" @click="openSmtp = !openSmtp" class="relative w-full bg-white border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
                                         <span class="block truncate" x-text="selectedSmtpName"></span>
                                         <span class="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
                                             <svg class="h-5 w-5 text-gray-400" fill="currentColor" viewBox="0 0 20 20"><path fill-rule="evenodd" d="M10 3a.75.75 0 01.55.24l3.25 3.5a.75.75 0 11-1.1 1.02L10 4.852 7.3 7.76a.75.75 0 01-1.1-1.02l3.25-3.5A.75.75 0 0110 3zm-3.76 9.2a.75.75 0 011.06.04l2.7 2.908 2.7-2.908a.75.75 0 111.1 1.02l-3.25 3.5a.75.75 0 01-1.1 0l-3.25-3.5a.75.75 0 01.04-1.06z" clip-rule="evenodd" /></svg>
